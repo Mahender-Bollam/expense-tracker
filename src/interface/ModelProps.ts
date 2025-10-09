c4d62ed91cecbbf21d58051403d369afbe552b3b
admin <admin@admins-MacBook-Pro-2.local>
2025-10-09 17:36:13 +0530

    Define modalCardProps interface for modalCard

--- src/interface/ModelProps.ts
+++ src/interface/ModelProps.ts
@@ -1,6 +1,23 @@
+import { MouseEventHandler } from "react";
+import { FormData } from "./FormData";
+
 
 export interface ModalProps {
   isOpen: boolean;
-  onClose: () => void;
+  onClose: ()=>void;
   children: React.ReactNode;
 }
+
+export interface ModelCardProps{
+    isModalOpen:boolean,
+    closeModal:()=>void,
+    formData:FormData,
+    setFormData:Function,
+    hoveredButton:HoveredButton,
+    setHoveredButton:Function,
+    editingId:number|null,
+    handleClick:MouseEventHandler<HTMLButtonElement>
+}
+
+export type HoveredButton = string | null;
+export type HoveredExpense = number | null;
