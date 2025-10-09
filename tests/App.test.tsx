7f0bca7dd25bb36778df5f576f4d6faaa12b9aba
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2025-10-09 19:42:33 +0530

    Update App test file

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+//import App from '../src/App';
+import ExpenseTracker from '../src/ExpenseTracker';
 
 it('renders expenseTracker Component', () => {
-  render(<App />);
+  render(<ExpenseTracker />);
   expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
