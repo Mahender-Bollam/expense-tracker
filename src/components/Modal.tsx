700aee1fd12d02b019545c735d8fecb74e3864e0
admin <admin@admins-MacBook-Pro-2.local>
2025-10-09 16:32:48 +0530

    Structure modal component in components

---
+++ src/components/Modal.tsx
@@ -0,0 +1,15 @@
+import { ModalProps } from "../interface/ModelProps";
+import { styles } from "../styles/ExpenseTracker";
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div style={styles.overlay} onClick={onClose}>
+      <div style={styles.modal} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
+};
+export default Modal;
