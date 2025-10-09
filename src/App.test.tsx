8e6a7218a30d0afb60f9035076c30cfbe04765aa
CHEPPALI KALYANI <kalyani.cheppali@everest.engineering>
2025-10-09 23:23:36 +0530

    Add test cases for app component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import ExpenseTracker from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+describe('Test cases for app component',()=>{
+ 
+  test('renders Manage your daily expenses efficiently in the document', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Manage your daily expenses efficiently/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("Should render expense trackor component",()=>{
+  render(<ExpenseTracker />)
+  const addExpenseButton = screen.getByRole('button',{name:'Add Expense'})
+  userEvent.click(addExpenseButton)
+})
+
+test("Should call setHoveredButton on hoverleave",()=>{
+  render(<ExpenseTracker />)
+  const addExpenseButton = screen.getByRole('button',{name:'Add Expense'})
+  fireEvent.mouseEnter(addExpenseButton)
+  fireEvent.mouseLeave(addExpenseButton)
+})
+test("Should call setHoveredButton on hoverleave in remove button",()=>{
+  render(<ExpenseTracker />)
+  const removeExpense = screen.getAllByRole('Remove')[0]
+  userEvent.click(removeExpense)
+  fireEvent.mouseLeave(removeExpense)
+  fireEvent.mouseEnter(removeExpense)
+})
+
+test("Should call setHoveredButton on hoverleave in edit button",()=>{
+  render(<ExpenseTracker />)
+  const removeExpense = screen.getAllByRole('edit')[0]
+  userEvent.click(removeExpense)
+  fireEvent.mouseLeave(removeExpense)
+  fireEvent.mouseEnter(removeExpense)
+  
+})
+
+})
+
+
+
+
