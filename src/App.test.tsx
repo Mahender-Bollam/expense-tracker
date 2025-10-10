c9dd915052ab0c6e8896b68718a18d9ece541497
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-10 15:27:47 +0530

    Add unit test for the Appcomponent rendeing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders  tittle element', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Expense Tracker/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders description of page',()=>{
+  render(<App/>);
+  expect(screen.getByText(/Manage your daily expenses efficiently/i)).toBeInTheDocument();
+})
