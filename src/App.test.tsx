c74cbe9fee80a8ce5e1659187ca63aaf6abeac0c
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-10 11:20:17 +0530

    Add testcase for to check the model is open properly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Expense Tracker/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Expense Tracker testcases", () => {
+  describe("UI testcases", () => {
+    test("renders learn react link", () => {
+      render(<App />);
+      const linkElement = screen.getByText(/Expense Tracker/i);
+      expect(linkElement).toBeInTheDocument();
+    });
+    test("I should open Modal properly" ,async()=>{
+      render(<App />);
+      const button = screen.getByRole("button" , {name: /Add Expense/});
+      await userEvent.click(button)
+      expect(screen.getByText("Add New Expense")).toBeInTheDocument();
+    })
+  });
 });
