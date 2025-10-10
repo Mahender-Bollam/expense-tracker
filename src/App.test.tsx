e2d227e3dc3a2d36ab030bc5386c0448e35891f4
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-10 12:30:03 +0530

    Add testcase to check the inputFields

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -17,4 +17,22 @@ describe("Expense Tracker testcases", () => {
       expect(screen.getByText("Add New Expense")).toBeInTheDocument();
     })
   });
+  describe("Functionsl Testcases" , ()=>{
+    test("Checking the input fields" , async()=>{
+      render(<App />)
+      const addExpenseBtn = screen.getByRole("button" , {name : /Add Expense/i});
+      userEvent.click(addExpenseBtn);
+      
+      const descriptionField = screen.getByPlaceholderText("Enter description")
+      const amountField = screen.getByPlaceholderText("0.00")
+      const categoryField = screen.getByText("Category")
+      const dateField = screen.getByTestId("date")
+
+      await userEvent.type(descriptionField , "Tea")
+      await userEvent.type(amountField , "200")
+      await userEvent.type(categoryField , "Food")
+      await userEvent.type(dateField , "10/10/2025")
+
+    })
+  })
 });
