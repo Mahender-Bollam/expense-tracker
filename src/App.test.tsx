a8789b30997a90f59a44ecbda7bde52241854f95
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-10-10 15:07:20 +0530

    Add testcase to check alert after adding expense

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -17,7 +17,8 @@ describe("Expense Tracker testcases", () => {
       expect(screen.getByText("Add New Expense")).toBeInTheDocument();
     })
   });
-  describe("Functionsl Testcases" , ()=>{
+  describe("Functional Testcases" , ()=>{
+
     test("Checking the input fields" , async()=>{
       render(<App />)
       const addExpenseBtn = screen.getByRole("button" , {name : /Add Expense/i});
@@ -34,5 +35,30 @@ describe("Expense Tracker testcases", () => {
       await userEvent.type(dateField , "10/10/2025")
 
     })
+  
+  test("it should add an expensive and trigger alert",async()=>{
+    const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
+
+      render(<App />)
+      const addExpenseBtn = screen.getByText("Add Expense");
+      await userEvent.click(addExpenseBtn);
+      expect(screen.getByText("Add New Expense")).toBeInTheDocument()
+      
+      const descriptionField = screen.getByPlaceholderText("Enter description")
+      const amountField = screen.getByPlaceholderText("0.00")
+      const categoryField = screen.getByText("Category")
+      const dateField = screen.getByTestId("date")
+
+      await userEvent.type(descriptionField , "Tea")
+      await userEvent.type(amountField , "200")
+      await userEvent.type(categoryField , "Food")
+      await userEvent.type(dateField , "10/10/2025") 
+      const button = screen.getAllByRole("button" , { name : "Add Expense"})
+      await userEvent.click(button[1])
+
+      expect(mockAlert).toHaveBeenCalled()
+
   })
+  })
+
 });
