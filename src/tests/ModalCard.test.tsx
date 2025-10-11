2f4c3bf47b385886d61a62098a9a5ac369f3eec5
admin <admin@admins-MacBook-Pro-2.local>
2025-10-11 13:39:19 +0530

    Update tests with changed props to component

--- src/tests/ModalCard.test.tsx
+++ src/tests/ModalCard.test.tsx
@@ -3,20 +3,18 @@ import { FormData } from "../interface/FormData";
 import { HoveredButton } from "../interface/ModelProps";
 import ModalCard from "../components/ModalCard";
 import userEvent from "@testing-library/user-event";
-import { styles } from "../styles/ExpenseTracker";
 
 describe("ModalCard",()=>{
     let isModalOpen=true;
     let closeModal=jest.fn();
-    let formData:FormData={
-        id:3,description:"Coffee",amount:"35",category:'Food',date:"10/10/2003"
-    }
+    let formData:FormData={id:3,description:"Coffee",amount:"35",category:'Food',date:"10/10/2003"}
     const setFormData=jest.fn();
     let hoveredButton:HoveredButton=null;
     let setHoveredButton=jest.fn();
     let editingId=null;
-    let handleClick=jest.fn()
-    const options={isModalOpen,closeModal,formData,setFormData,hoveredButton,setHoveredButton,editingId,handleClick}
+    let updateExpense=jest.fn()
+    let addExpense=jest.fn()
+    const options={isModalOpen,closeModal,formData,setFormData,hoveredButton,setHoveredButton,editingId,addExpense,updateExpense}
     test("should click on closeModal",()=>{
         render(<ModalCard {...options}/>)
         const closeButton=screen.getByTestId("close-button")
@@ -24,6 +22,7 @@ describe("ModalCard",()=>{
         fireEvent.mouseEnter(closeButton)
         fireEvent.mouseLeave(closeButton)
     })
+
     test("should change input value on change",()=>{
         render(<ModalCard {...options}/>)
         const description=screen.getByPlaceholderText("Enter description");
@@ -33,34 +32,31 @@ describe("ModalCard",()=>{
         const category=screen.getByTestId("select-category");
         userEvent.selectOptions(category,"Food") 
         const date=screen.getByTestId("date-input");
+        userEvent.clear(date)
         userEvent.type(date,"2025-10-05")  
+        expect(setFormData).toHaveBeenCalled()
     })
+
     test("should click updateExpense button with editingId",()=>{
         const updatedOptions={...options,editingId:formData.id}
         render(<ModalCard {...updatedOptions}/>)
         const clickButton=screen.getByRole("button",{name:"Update Expense"})
-        userEvent.click(clickButton)        
+        fireEvent.click(clickButton) ;
     })
-    test("should click addExpense with editingId null",()=>{
+
+    test("should click addExpense with editingId null",async ()=>{
         render(<ModalCard {...options}/>)
         const clickButton=screen.getByRole("button",{name:"Add Expense"})
-        userEvent.click(clickButton);
+        fireEvent.click(clickButton);
         fireEvent.mouseEnter(clickButton)
-        fireEvent.mouseLeave(clickButton)       
+        fireEvent.mouseLeave(clickButton)  
     })
+
     test("should click cancel to close modal",()=>{
         render(<ModalCard {...options}/>)
         const cancel=screen.getByRole("button",{name:"Cancel"})
         userEvent.click(cancel);
-        fireEvent.mouseEnter(cancel)
-        fireEvent.change(cancel,{target:{value:"submit"}});
-        // expect(cancel).toHaveStyle({styles,hoveredButton:'submit'})    
+        fireEvent.mouseEnter(cancel) 
         fireEvent.mouseLeave(cancel);
-        
     })
-
-    // test("should check hover button styles having content",()=>{
-    //     render(<ModalCard {...options}/>);
-    //     const styleElement=screen.getByRole
-    // })
 }) 
