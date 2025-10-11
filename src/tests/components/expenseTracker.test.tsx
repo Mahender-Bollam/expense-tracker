a5831d5b3112714dfa4125e22f63fdf460f63435
uma <umamaheswari.g@everest.engineering>
2025-10-11 16:05:03 +0530

    Add test case of open modal for add expense

--- src/tests/components/expenseTracker.test.tsx
+++ src/tests/components/expenseTracker.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import ExpenseTracker from "../../components/expenseTracker";
 import "@testing-library/jest-dom";
 
@@ -10,6 +10,12 @@ describe("ExpenseTracker Component", () => {
     expect(screen.getByText("Groceries")).toBeInTheDocument();
     expect(screen.getByText("Gas")).toBeInTheDocument();
     expect(screen.getByText("$130.50")).toBeInTheDocument();
+  });
+
+  test("opens Add Expense modal when Add Expense button is clicked", () => {
+    render(<ExpenseTracker />);
+    fireEvent.click(screen.getByText("Add Expense"));
+    expect(screen.getByText("Add New Expense")).toBeInTheDocument();
   });
 });
 
