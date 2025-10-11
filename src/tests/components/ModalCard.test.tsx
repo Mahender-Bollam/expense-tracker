0f1e85e359d6883b64230b04065e8947852eb945
NivedithaGunapati <niveditha.gunapati@everest.engineering>
2025-10-11 19:28:28 +0530

    Add test to check the expense addition successfully

---
+++ src/tests/components/ModalCard.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import ModalCard from "../../components/ModalCard"
+import Modal from "../../components/ModalComponent"
+import { AppWindow } from "lucide-react"
+import ExpenseTracker from "../../App"
+import userEvent from "@testing-library/user-event"
+
+const mockExpenses = [{ id: 1, description: 'Groceries', amount: 85.50, category: 'Food', date: '2025-10-05' }]
+
+const mockFormData = {
+    id: 2, description: 'Self', amount: '85', category: 'Shopping', date: '2025-10-10'
+}
+
+const mockSetExpenses = jest.fn()
+const mockHoveredButton = ''
+const mockSetHoveredButton = jest.fn()
+const mockSetFormData = jest.fn()
+const mockSetIsModalOpen = jest.fn()
+const mockCloseModal = jest.fn
+window.alert = jest.fn()
+
+
+
+const renderComponent = (props={}) => {
+    render(
+        <ModalCard expenses={mockExpenses} setExpenses={mockSetExpenses}
+            hoveredButton={mockHoveredButton} setHoveredButton={mockSetHoveredButton}
+            setFormData={mockSetFormData}
+            setIsModalOpen={mockSetIsModalOpen} isModalOpen={false}
+            closeModal={mockCloseModal} editingId={null} formData={mockFormData} {...props}/>
+    )
+}
+
+
+describe('Modal Card component', () => {
+
+    test('Should add the expense', () => {
+        renderComponent({isModalOpen:true})
+        fireEvent.change(screen.getByPlaceholderText(/Enter description/i), { target: { value: 'Self' } });
+        fireEvent.change(screen.getByPlaceholderText(/0.00/i), { target: { value: '260' } });
+        fireEvent.change(screen.getByText(/Select Category/i), { target: { value: 'Shopping' } })
+        fireEvent.click(screen.getByRole('button',{name: /Add Expense/i}))
+    })
+   
+})
+
+
