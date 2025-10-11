08dc3a10c4e28e3c4b2f3299e5a497212db458fb
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2025-10-11 15:29:29 +0530

    Add Modal Component file

--- tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-//import App from '../src/App';
-import ExpenseTracker from '../src/ExpenseTracker';
-
-it('renders expenseTracker Component', () => {
-  render(<ExpenseTracker />);
-  expect(screen.getByText('Learn React')).toBeInTheDocument();
-});
