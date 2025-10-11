ee393b9980640b4114787ff62ecefe604afddb97
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-12 00:55:04 +0530

    Add unit test for close modal button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import ExpenseTracker from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders  tittle element', () => {
   render(<App />);
@@ -17,3 +18,12 @@ test(' display the total amount correctly', () => {
     expect(screen.getByText('Total Expenses')).toBeInTheDocument();
     expect(screen.getByText('$130.50')).toBeInTheDocument(); 
   });
+
+test('closes modal when "Close Modal" button is clicked',() => {
+  render(<App />);
+  userEvent.click(screen.getByText(/Add Expense/i));
+  userEvent.click(screen.getByRole('button', { name: /close modal/i }));
+  expect(screen.queryByText(/Add New Expense/i)).not.toBeInTheDocument();
+});
+
+  
