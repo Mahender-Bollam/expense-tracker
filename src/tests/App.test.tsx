210741ea3cac4251de77033131478a2b287c9f9c
NivedithaGunapati <niveditha.gunapati@everest.engineering>
2025-10-11 19:00:54 +0530

    Add test to check the app component

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+describe('App Component',()=>{
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Expense Tracker/i);
   expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Manage your daily expenses efficiently/i)).toBeInTheDocument()
 });
+
+
+})
+
+
