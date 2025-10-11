96f709a7640c973490f05e7dc4540f574bd812e9
uma <umamaheswari.g@everest.engineering>
2025-10-11 22:39:55 +0530

    Add test case to display input fields

---
+++ src/tests/components/expenseForm.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import ExpenseForm from "../../components/expenseForm";
+import "@testing-library/jest-dom";
+import { Expense } from "../../interface/expense";
+import { FormData } from "../../interface/formData";
+
+describe("ExpenseForm Component", () => {
+  const mockSetFormData = jest.fn();
+  const mockSetExpenses = jest.fn();
+  const mockCloseModal = jest.fn();
+  const mockSetHoveredButton = jest.fn();
+
+  const baseProps = {
+    formData: {
+      description: "",
+      amount: "",
+      category: "",
+      date: "",
+    } as FormData,
+
+    setFormData: mockSetFormData,
+    categories: ["Food", "Transport", "Bills"],
+    editingId: null,
+    setExpenses: mockSetExpenses,
+    expenses: [] as Expense[],
+    closeModal: mockCloseModal,
+    hoveredButton: null,
+    setHoveredButton: mockSetHoveredButton,
+    styles: {},
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("display all input fields", () => {
+    render(<ExpenseForm {...baseProps} />);
+    expect(screen.getByText("Description")).toBeInTheDocument();
+    expect(screen.getByText("Amount")).toBeInTheDocument();
+    expect(screen.getByText("Category")).toBeInTheDocument();
+    expect(screen.getByText("Date")).toBeInTheDocument();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
