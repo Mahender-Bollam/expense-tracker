60fa5105901c3c9dcb5337b7d62d5a1407c96873
Suhail <suhail.shaik@everest.engineering>
2025-10-12 15:29:00 +0530

    Test to check error on empty fields

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -31,4 +31,23 @@ describe("Expense tracker" ,() =>{
     const expense =screen.getByText("Pizza");
     expect(expense).toBeInTheDocument();
   })
+  it("Should throw errors on empty fields", () =>{
+    const alertMock = jest.spyOn(window,'alert').mockImplementation();
+     render(
+      <App/>
+    );
+    const addButton = screen.getByTestId('open-model');
+    fireEvent.click(addButton);
+    const desccription = screen.getByPlaceholderText("Enter description");
+    const amount = screen.getByPlaceholderText("0.00");
+    const category = screen.getByDisplayValue("Select category");
+    const date = screen.getByLabelText("Date");
+    fireEvent.change(desccription,{target : {value:""}});
+    fireEvent.change(amount,{target : {value:"20"}});
+    fireEvent.change(category,{target : {value:"Food"}});
+    fireEvent.change(date,{target : {value:"2025-10-10"}});
+    const button = screen.getByTestId('add-expense');
+    fireEvent.click(button);
+    expect(alertMock).toHaveBeenCalled();
+  })
 })
