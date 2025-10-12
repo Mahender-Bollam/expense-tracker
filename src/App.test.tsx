4134eb2f8f26ff244ccd8e6c46923b600fa3c654
Suhail <suhail.shaik@everest.engineering>
2025-10-12 15:10:30 +0530

    Test to add new expense

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Expense tracker" ,() =>{
+  it("Should render header correctly",() => {
+    render(
+      <App/>
+    );
+    const title = screen.getByText("Expense Tracker");
+    const addButton = screen.getByRole('button' ,{name:"Add Expense"} )
+    expect(addButton).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+  })
+  it("Should add new expense", () => {
+    render(
+      <App/>
+    );
+    const addButton = screen.getByTestId('open-model');
+    fireEvent.click(addButton);
+    const desccription = screen.getByPlaceholderText("Enter description");
+    const amount = screen.getByPlaceholderText("0.00");
+    const category = screen.getByDisplayValue("Select category");
+    const date = screen.getByLabelText("Date");
+    fireEvent.change(desccription,{target : {value:"Pizza"}});
+    fireEvent.change(amount,{target : {value:"20"}});
+    fireEvent.change(category,{target : {value:"Food"}});
+    fireEvent.change(date,{target : {value:"2025-10-10"}});
+    const button = screen.getByTestId('add-expense');
+    fireEvent.click(button);
+    const expense =screen.getByText("Pizza");
+    expect(expense).toBeInTheDocument();
+  })
+})
