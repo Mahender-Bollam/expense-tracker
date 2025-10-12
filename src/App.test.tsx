243a34ae536778d7fdbc8ce578d6efbd96d2cfb6
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-12 21:06:30 +0530

    Add unit test for input form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
 import App from './App';
 import ExpenseTracker from './App';
 import userEvent from '@testing-library/user-event';
@@ -26,4 +26,20 @@ test('closes modal when "Close Modal" button is clicked',() => {
   expect(screen.queryByText(/Add New Expense/i)).not.toBeInTheDocument();
 });
 
-  
+test('correctly handles the expense form', async () => {
+  render(<App />);
+  const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => { });
+  fireEvent.click(screen.getAllByText(/Add Expense/i)[0]);  
+  fireEvent.change(screen.getByPlaceholderText('Enter description'), { target: { value: 'Hyderbadbiryani' } });
+  fireEvent.change(screen.getByPlaceholderText('0.00'), { target: { value: '250' } });
+  fireEvent.change(screen.getByLabelText('Category'), { target: { value: 'Food' } });
+  fireEvent.change(screen.getByLabelText('Date'), { target: { value: '2025-10-07' } });
+  expect(screen.getByText(/Add New Expense/i)).toBeInTheDocument();
+  fireEvent.click(screen.getAllByText(/Add Expense/i)[1]); 
+  expect(alertMock).toHaveBeenCalledWith('Add Expense  successfull');
+  expect(screen.getByText('Hyderbadbiryani')).toBeInTheDocument();
+
+   
+
+});
+
