3ed7b6b5e3a54a9c65df04b9d120b1fdd21935be
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-21 02:32:55 +0530

    Add editexpenseapi functionality

--- src/serverapi.ts
+++ src/serverapi.ts
@@ -17,3 +17,19 @@ export const addExpense = async (expenseData: Expense) => {
         throw error; 
     }
 };
+export const editExpense = async (expenseData: Expense,id:Number) => { 
+    try {
+        const response = await fetch(`${apiData}/${id}`, { 
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(expenseData),
+        });
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.error('Error adding expense:', error); 
+        throw error; 
+    }
+};
