c93edcc3d2cd2b6c790b5f96c4133dd436cf1079
CHEPPALI KALYANI <kalyani.cheppali@everest.engineering>
2025-10-20 21:45:04 +0530

    Implement expense functionalities with API.

---
+++ src/api/expenseAPI.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { Expense } from '../components/ExpenseTrackor';
+
+const API_URL = 'http://localhost:3005'; 
+
+export const getExpenses = async () => {
+  const response = await axios.get(`${API_URL}/expenses`);
+  return response.data.data as Expense[];
+};
+
+export const getExpenseById = async (id: string) => {
+  const response = await axios.get(`${API_URL}/expenses/${id}`);
+  return response.data.data as Expense;
+};
+
+export const addExpense = async (expense: Expense) => {
+  const response = await axios.post(`${API_URL}/expenses`, expense);
+  return response.data;
+};
+
+export const updateExpense = async (expense: Expense) => {
+  const response = await axios.put(`${API_URL}/expenses/${expense.id}`, expense);
+  return response.data;
+};
+
+export const deleteExpense = async (id: string) => {
+  const response = await axios.delete(`${API_URL}/expenses/${id}`);
+  return response.data;
+};
