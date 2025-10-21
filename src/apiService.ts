b2b6dcbb4c3c1ce024fafc4969d5b3e40a9828ff
Suhail <suhail.shaik@everest.engineering>
2025-10-21 16:08:40 +0530

    Get all expense from firebase

---
+++ src/apiService.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+const API = "http://localhost:3000";
+export const getData = async () => {
+    const response = await axios.get(`${API}/expense`);
+    if(!response){
+        throw new Error("Failed to load")
+    }
+    return response.data;
+} 
+
