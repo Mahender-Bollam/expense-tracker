74dc3c260e008544b8c9ffa760e96c610905cdf1
Suhail <suhail.shaik@everest.engineering>
2025-10-21 16:16:00 +0530

    Request to add new expense

--- src/apiService.ts
+++ src/apiService.ts
@@ -2,9 +2,26 @@ import axios from 'axios';
 const API = "http://localhost:3000";
 export const getExpense = async () => {
     const response = await axios.get(`${API}/expense`);
-    if(!response){
+    if (!response) {
         throw new Error("Failed to load")
     }
     return response.data;
-} 
+}
 
+export const postData = async (id: number, expense: {
+    description: string,
+    category: string,
+    amount: number,
+}) => {
+    const response = await axios.post(`${API}/expense/${id}`, expense, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if (response) {
+        console.log("Expense Added")
+    }
+    else {
+        console.log("Failed to push")
+    }
+}
