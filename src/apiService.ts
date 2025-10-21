8a5cf99113312df2650b373a1c3a1872a809d7d7
Suhail <suhail.shaik@everest.engineering>
2025-10-21 16:13:49 +0530

    Provide proper function name

--- src/apiService.ts
+++ src/apiService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = "http://localhost:3000";
-export const getData = async () => {
+export const getExpense = async () => {
     const response = await axios.get(`${API}/expense`);
     if(!response){
         throw new Error("Failed to load")
